94fc16fea3518b24b8df2f577f3a6d4a3932def3
Alecx Moritz <alecxmoritz7@gmail.com>
2019-02-24 21:55:52 -0500

    stubbed

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,17 +1,36 @@
 const router = require('express').Router();
 const Item = require('../models/index').sequelize.import('../models/item');
+const validateSession = require('../middleware/validate-session');
 
 // post
+router.post('/', validateSession, (req, res) => {
+
+});
 
 // get by id
+router.get('/:id', validateSession, (req, res) => {
+    
+});
 
 // get all
+router.get('/', validateSession, (req, res) => {
+    
+});
 
 // get array of ids
+router.get('/list', validateSession, (req, res) => {
+
+});
 
 // update
+router.put('/:id', validateSession, (req, res) => {
+    
+});
 
 // delete
+router.delete('/:id', validateSession, (req, res) => {
+    
+});
 
 
 module.exports = router;
